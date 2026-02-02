e83d68e4437d9fe0a86b182a038784e596097d80
23080ad <23080ad@hvhs.school.nz>
2026-02-03 12:18:48 +1300

    button touch ups

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
 function Buttonfunction() {
 let html = document.getElementById("script_run").innerHTML = alert('Congratulations!\nYou got the script running.');
-
+document.getElementById("script_run").innerHTML = "Click me!";
 }
